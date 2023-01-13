e70d61e6e2f30296891c5bb769cae26bace98828
adevmir <ademirvalonqar@gmail.com>
2023-01-13 14:39:37 -0300

    requisito 10 - corrigindo percentual ao invéz de quantidade

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,3 +1,3 @@
 db.produtos.find({
     valoresNutricionais: { $elemMatch:
-        { tipo: "proteínas", quantidade: { $gte: 30, $lte: 40 } } } }, { nome: 1, _id: 0 });
+        { tipo: "proteínas", percentual: { $gte: 30, $lte: 40 } } } }, { nome: 1, _id: 0 });
