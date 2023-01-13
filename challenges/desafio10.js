81e039300a0d44a959988e9e6ec6cb5241abe05f
adevmir <ademirvalonqar@gmail.com>
2023-01-13 14:37:23 -0300

    requisito 10 - corrigindo lint

--- challenges/desafio10.js
+++ challenges/desafio10.js
@@ -1,1 +1,3 @@
-db.produtos.find({ valoresNutricionais: { $elemMatch: { tipo: "proteínas", quantidade: { $gte: 30, $lte: 40 } } } }, { nome: 1, _id: 0 });
+db.produtos.find({
+    valoresNutricionais: { $elemMatch:
+        { tipo: "proteínas", quantidade: { $gte: 30, $lte: 40 } } } }, { nome: 1, _id: 0 });
